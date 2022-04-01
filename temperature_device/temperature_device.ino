e3ee92aa3f7aaf7768f55ee3b15925e8af8ffc63
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-04-01 03:46:24 -0300

    feat: timmer for cooking

--- temperature_device/temperature_device.ino
+++ temperature_device/temperature_device.ino
@@ -2,18 +2,11 @@
 #include <DallasTemperature.h>
 #include <ESP8266WiFi.h>         //https://github.com/esp8266/Arduino
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager​
-float setMaxTemperatureCokking = 60;
-bool isCooking = true;
+#include "temperature.h"
 
-OneWire barramento(D4);
-DallasTemperature sensor(&barramento);
-
-float getTemperature(DallasTemperature sensor) {
-  sensor.requestTemperatures();
-  return sensor.getTempCByIndex(0);
-
-}
-
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
+unsigned long timingCooking = 300000;
 void setup()
 {
    Serial.begin(115200);
@@ -21,21 +14,30 @@ void setup()
    WiFiManager wifiManager;
    wifiManager.autoConnect("SousVideHomeMade");
    Serial.println("connected :)");
+   startMillis = millis();  //initial start time
 
 }
 
 void loop()
 {
   while (isCooking) {
-    float temp = getTemperature(sensor);
-    if (temp >= setMaxTemperatureCokking) {
-      Serial.println("DEACTIVATE RELE COOKING POT");
-      Serial.println(temp);
-    } else {
-      Serial.println("ACTIVATE RELE COOKING POT");
-      Serial.println(temp);
+    currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+    Serial.println(currentMillis);
+    Serial.println(timingCooking);
+    if (currentMillis <= timingCooking) {
+      
+        float temp = getTemperature(sensor);
+        if (temp >= setMaxTemperatureCokking) {
+          Serial.println("DEACTIVATE RELE COOKING POT");
+          Serial.println(temp);
+        } else {
+          Serial.println("ACTIVATE RELE COOKING POT");
+          Serial.println(temp);
+          }
+          
+          delay(1000);
+          }  else {
+      isCooking = false;
     }
-
-    delay(1000);
-  }
+    }
 }

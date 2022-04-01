4c7b912d086f52b358e8513c55369fec63c37107
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-04-01 01:33:47 -0300

    feat: wifiManager

--- temperature_device/temperature_device.ino
+++ temperature_device/temperature_device.ino
@@ -1,9 +1,28 @@
-#include "temperature.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP8266WiFi.h>         //https://github.com/esp8266/Arduino
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager​
+float setMaxTemperatureCokking = 60;
+bool isCooking = true;
+
+OneWire barramento(D4);
+DallasTemperature sensor(&barramento);
+
+float getTemperature(DallasTemperature sensor) {
+  sensor.requestTemperatures();
+  return sensor.getTempCByIndex(0);
+
+}
 
 void setup()
 {
    Serial.begin(115200);
-   sensor.begin(); 
+   sensor.begin();
+   WiFiManager wifiManager;
+   wifiManager.resetSettings();
+   wifiManager.autoConnect("SousVideHomeMade");
+   Serial.println("connected :)");
+
 }
 
 void loop()

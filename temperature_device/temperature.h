8ca31c5371dd774747d4c45ca53bfd4c49cfed5e
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-07-15 02:24:57 -0300

    refactor: class TimerCooker now is managed the elapsed time to cook

--- temperature_device/temperature.h
+++ temperature_device/temperature.h
@@ -1,10 +1,11 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#define D3 0
 
 float setMaxTemperatureCokking = 60;
 bool isCooking = true;
 
-OneWire barramento(D4);
+OneWire barramento(D3);
 DallasTemperature sensor(&barramento);
 
 float getTemperature(DallasTemperature sensor) {
--- temperature_device/temperature_device.ino
+++ temperature_device/temperature_device.ino
@@ -7,6 +7,32 @@
 unsigned long startMillis;  //some global variables available anywhere in the program
 unsigned long currentMillis;
 unsigned long timingCooking = 300000;
+bool isReleActivate = false;
+
+class TimerCooker {
+
+  private:
+    unsigned long startTime;
+    unsigned long timingCooking;
+
+  public:
+  TimerCooker(unsigned long timeToCook) {
+    timingCooking = timeToCook;
+  }
+
+  void startTimer() {
+    startTime = millis();
+  }
+
+  bool isTimeUp() {
+    unsigned long elapsedMillis = millis() - startTime;
+    return (elapsedMillis >= timingCooking);
+  }
+
+};
+
+TimerCooker timer_cooker(60000);
+
 void setup()
 {
    Serial.begin(115200);
@@ -14,30 +40,44 @@ void setup()
    WiFiManager wifiManager;
    wifiManager.autoConnect("SousVideHomeMade");
    Serial.println("connected :)");
-   startMillis = millis();  //initial start time
+   timer_cooker.startTimer();
 
 }
+
+
 
 void loop()
 {
-  while (isCooking) {
-    currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
-    Serial.println(currentMillis);
-    Serial.println(timingCooking);
-    if (currentMillis <= timingCooking) {
-      
-        float temp = getTemperature(sensor);
-        if (temp >= setMaxTemperatureCokking) {
-          Serial.println("DEACTIVATE RELE COOKING POT");
-          Serial.println(temp);
-        } else {
-          Serial.println("ACTIVATE RELE COOKING POT");
-          Serial.println(temp);
-          }
-          
-          delay(1000);
-          }  else {
-      isCooking = false;
-    }
-    }
+  if (timer_cooker.isTimeUp()) {
+    Serial.println("Time's up!!");
+  } else {
+    Serial.println("Still cooking!!");
+  }
 }
+
+
+//  while (isCooking) {
+//    currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+//
+//    timer_cooker.execute_time();
+//    if (currentMillis <= timingCooking) {
+//     
+//        float temp = getTemperature(sensor);
+ //       Serial.println(temp);
+ //       if (temp >= setMaxTemperatureCokking) {
+   //       Serial.println("DEACTIVATE RELE COOKING POT");
+     //     isReleActivate = false;
+       // } else {
+         // if (!isReleActivate) {
+           //   Serial.println("ACTIVATE RELE COOKING POT");
+             // isReleActivate = true;
+
+          //} else {
+          //}
+          //}
+          
+          //}  else {
+      //isCooking = false;
+    //}
+    //delay(1000);
+    //}

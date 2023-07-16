0c729f0fa05013dadd47a339a07aa33c31fc9852
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-07-16 16:35:25 -0300

    feat: SousVideOrchestrator

---
+++ main/sous_vide_orchestrator.h
@@ -0,0 +1,47 @@
+
+
+class SousVideOrchestrator {
+
+
+    private:
+       TemperatureSensor& temperatureSensor;
+       TimerCooker& timerCooker;
+       CookingPot& cookingPot;
+       bool isTimerStarted;
+       const float MAX_TEMPERATURE_COOKING;
+       const float START_THRESHOLD;
+
+    
+    public:
+      SousVideOrchestrator(TemperatureSensor& temperatureSensor, TimerCooker& timerCooker, CookingPot& cookingPot, float maxTemperature) 
+      : temperatureSensor(temperatureSensor), timerCooker(timerCooker), cookingPot(cookingPot), MAX_TEMPERATURE_COOKING(maxTemperature), START_THRESHOLD(maxTemperature * 0.9) {
+        this->isTimerStarted = false;
+      }
+
+
+      void setup() {
+        temperatureSensor.setup();
+        cookingPot.setup();
+      }
+
+      void update() {
+        float temperature = temperatureSensor.getTemperature();
+
+        if (!isTimerStarted && temperature >= START_THRESHOLD) {
+          timerCooker.startTimer();
+          isTimerStarted = true;
+        }
+
+        if (isTimerStarted) {
+            cookingPot.isReachMaxTemperature(temperature);
+            cookingPot.checkRele();
+            timerCooker.printRemainingTime();
+
+            if (timerCooker.isTimeUp()) {
+              Serial.println("Time's up!!");
+              isTimerStarted = false;
+            }
+
+        }
+      }
+};

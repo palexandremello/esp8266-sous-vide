1713f6b9d1a3c31b1476bb4d84fbeaff3f3d21ed
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-07-26 00:31:02 -0300

    feat: MQtt libraries

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,8 @@
 #include <DallasTemperature.h>
 #include <ESP8266WiFi.h>         //https://github.com/esp8266/Arduino
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager​
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
 #include "temperature.h"
 #include "cooking_pot.h"
 #include "timer.h"
@@ -10,6 +12,81 @@
 #define LED_PIN 16
 #define MAX_TEMPERATURE 63.0
 unsigned long timingCooking = 3600000;
+
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  uint16_t packetIdSub = mqttClient.subscribe("sousvide/temperature", 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+  mqttClient.publish("test/lol", 0, true, "test 1");
+  Serial.println("Publishing at QoS 0");
+  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+  Serial.print("Publishing at QoS 1, packetId: ");
+  Serial.println(packetIdPub1);
+  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+  Serial.print("Publishing at QoS 2, packetId: ");
+  Serial.println(packetIdPub2);
+
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
 
 
 TimerCooker timer_cooker(timingCooking);
@@ -24,6 +101,14 @@ void setup()
    wifiManager.autoConnect("SousVideHomeMade");
    Serial.println("connected :)");
    sous_vide_orchestrator.setup();
+   mqttClient.onConnect(onMqttConnect);
+   mqttClient.onDisconnect(onMqttDisconnect);
+   mqttClient.onSubscribe(onMqttSubscribe);
+   mqttClient.onUnsubscribe(onMqttUnsubscribe);
+   mqttClient.onMessage(onMqttMessage);
+   mqttClient.onPublish(onMqttPublish);
+   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
 
 }
 

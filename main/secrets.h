eb1234beecaca1f7694b139386c3a6ee4012adc5
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-19 20:54:57 -0300

    Revert "Delete secrets.h"
    This reverts commit bd6ee4eb13e63fe6c92ac743b1d03061bd3241b9.

---
+++ main/secrets.h
@@ -0,0 +1,9 @@
+#define MQTT_SERVER "143.198.229.205"
+#define MQTT_PORT 1883
+#define MQTT_SOUSVIDE_TEMPERATURE "sousvide/temperature"
+#define MQTT_PARAMS_COOKING "sousvide/params/cooking"
+#define WIFI_SSID "HUAWEI-2.4G-2xvw"
+#define WIFI_PASSWORD "hzupeJYw"
+#define MQTT_USER "pmello"
+#define MQTT_PASSWORD "147258Loki"
+#define AUTO_AP_SOUSVIDE "SousVideHomeMade"

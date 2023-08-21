98cd1221e4af35b821fec45aec09c37f65939b3f
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-21 03:32:36 -0300

    feat: ControlState class

--- main/main.ino
+++ main/main.ino
@@ -21,7 +21,7 @@ unsigned long timingCooking =  0;
 
 // Initialize MQTTManager
 MQTTManager mqttManager(MQTT_SERVER);
-PIDController controller(2, 0.80, 0);
+PIDController controller(1, 0.4, 0);
 TimerCooker timer_cooker(timingCooking);
 TemperatureSensor temperature_sensor;
 CookingPot cooking_pot(RELE_PIN);

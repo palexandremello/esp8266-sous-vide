b845ff072baf074162caf3e04fb0e8475e2cb27b
Paulo Alexandre Mello <34449770+palexandremello@users.noreply.github.com>
2023-10-15 21:13:57 -0300

    Update secrets.h

--- main/secrets.h
+++ main/secrets.h
@@ -1,13 +1,1 @@
-#define MQTT_SERVER "143.198.229.205"
-#define MQTT_PORT 1883
-#define MQTT_SOUSVIDE_TEMPERATURE "sousvide/temperature"
-#define MQTT_PARAMS_COOKING "sousvide/params/cooking"
-#define WIFI_SSID "HUAWEI-2.4G-2xvw"
-#define WIFI_PASSWORD "hzupeJYw"
-#define MQTT_USER "pmello"
-#define MQTT_PASSWORD "147258Loki"
-#define AUTO_AP_SOUSVIDE "SousVideHomeMade"
-#define SET_TEMPERATURE_TOPIC "sousvide/setTemperature"
-#define SET_TIMER_TOPIC "sousvide/setTimer"
-#define START_COOKING_TOPIC "sousvide/startCooking"
-#define PID_METRICS "sousvide/pidMetrics"
+
